540bd077b5c947e49bbfafc364bb68cc737703b0
DESKTOP-9PCJ6PM\artem <artik.kostko@gmail.com>
2021-05-11 23:56:43 +0300

    no message

--- SportFit/ClientApp/src/components/programsPage.js
+++ SportFit/ClientApp/src/components/programsPage.js
@@ -1,4 +1,4 @@
-﻿import React, { useEffect } from 'react';
+﻿import React, { useEffect, useState } from 'react';
 import {connect} from "react-redux";
 import * as programActions from '../actions/program';
 
